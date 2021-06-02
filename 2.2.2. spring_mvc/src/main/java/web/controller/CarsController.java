040c6a0d115f8591c77854b88de3689e16bf3228
89104775491Ds <dan-sivod@mail.ru>
2021-06-02 19:22:22 +0300

    add Car

---
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -0,0 +1,14 @@
+package web.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CarsController {
+
+    @GetMapping("/cars")
+    public String printCars(){
+        return "Cars";
+    }
+}
