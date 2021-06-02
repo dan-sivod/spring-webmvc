fc60e200305cc8a90c1bd133db30a08cfe5e2b72
danilasivodedov <dan-sivod@mail.ru>
2021-06-02 21:40:43 +0300

    правки вывода cars

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -1,14 +1,31 @@
 package web.controller;
 
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CarsController {
 
-    @GetMapping("/cars")
-    public String printCars(){
-        return "Cars";
+    @GetMapping(value = "/cars")
+    public String printCars(ModelMap model){
+        Car car1 = new Car("Camry", 40, "Toyota");
+        Car car2 = new Car("M3", 50, "BMW");
+        Car car3 = new Car("C180", 60, "Mercedes");
+        Car car4 = new Car("Supra", 30, "Toyota");
+        Car car5 = new Car("Logan", 7, "Reno");
+        List<Car> cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        cars.add(car5);
+        model.addAttribute("cars", cars);
+
+        return "cars";
     }
 }
---
+++ 2.2.2. spring_mvc/src/main/java/web/model/Car.java
@@ -0,0 +1,43 @@
+package web.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+
+    private String model;
+    private int price;
+    private String fabricator;
+
+    Car(){}
+
+    public Car(String model, int price, String fabricator) {
+        this.model = model;
+        this.price = price;
+        this.fabricator = fabricator;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getFabricator() {
+        return fabricator;
+    }
+
+    public void setFabricator(String fabricator) {
+        this.fabricator = fabricator;
+    }
+}
